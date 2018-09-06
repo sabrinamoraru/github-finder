1537e97015e0272b31fd42f0497dbeef1c354ef1
Sabrina Moraru <sabrina.moraru@gmail.com>
2018-09-06 15:41:01 +0300

    first commit

---
+++ github.js
@@ -0,0 +1,32 @@
+class Github {
+  constructor() {
+    this.client_id = "408d13e49f2a35b76ace";
+    this.client_secret = "dc9443991f2767ca7040b0cf34b5498475919255";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
